07accc800ab8468334d0e6935963ff0e6b488c37
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-19 21:20:12 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,7 +1,12 @@
 class LandmarksController < ApplicationController
 
-  get "/landmarks/new" do 
+  get "/landmarks/new" do
     erb :"landmarks/new"
   end
+
+  post "/landmarks" do
+    @landmark = Landmark.create(params[:landmark])
+  end
+
 
 end
